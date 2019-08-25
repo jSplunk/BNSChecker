a26af5281d3f01dca37fdf8d8e42161414fcb24a
Joel Gessner <joelxxzz@gmail.com>
2019-08-25 17:33:02 +0200

    First Commit
    Added .py file, and associated audio
    
    countdown.wav 
    
    I converted the file format to .wav from .flac for it to work with my script.
    
    Cyborg Countdown http://www.freesound.org/people/qubodup/sounds/211992 by Iwan ‘qubodup’ Gabovitch http://freesound.org/people/qubodup under CC-BY 3.0 License http://creativecommons.org/licenses/by/3.0/legalcode

---
+++ BNSChecker.py
@@ -0,0 +1,50 @@
+import psutil
+import os, sys
+from playsound import playsound
+
+found = False
+counter = 0
+amount_of_minutes_utill_kill = 120
+BNS = []
+warning_sound = "countdown.wav"
+one_minute_remaining = "underaminute.mp3"
+
+while(True):
+    print("Looking for processes...")
+    while(not found):
+        counter = 0
+        for p in psutil.process_iter(): #Search through all active processes
+            if p.name() in ["Client.exe", "NCLauncher2.exe"] and not p in BNS: #We find the process we are looking for
+                print(p, "found")
+                killed = False
+                BNS += [psutil.Process(p.pid)]
+                print("Addded ",p.name(), " with the PID: ", p.pid, "to the list")
+            if len(BNS) > 1:
+                found = True;
+                print("Found all occurences...")
+                break
+
+    print("[Started Counter}")
+    while(found):
+        counter = BNS[1].cpu_times().user
+        if (counter > amount_of_minutes_utill_kill * 60):
+            print("[Finished Counter]")
+            for process in BNS:
+                print("Killed: ", process.name())
+                process.kill()
+            print(counter)
+            if input("Press any key to exit..."): 
+                sys.exit()
+        elif (counter >= (amount_of_minutes_utill_kill * 60) - 11):
+            print("Final Call!")
+            playsound(warning_sound)
+        elif (counter >= (amount_of_minutes_utill_kill * 60) - 60):
+            print("Warning!")
+            playsound(one_minute_remaining)
+    
+
+
+            
+
+
+
