31179983f6e201a8a9f1a097fc083982af55987d
Joel Gessner <joelxxzz@gmail.com>
2019-08-25 17:44:40 +0200

    .BAT file for launching the game and the script

---
+++ BNSSTART.bat
@@ -0,0 +1,3 @@
+REM You can always change the path of either BNSChecker or the path of the game if it is not the same on you system
+start cmd /c python C:\BNSChecker\BNSChecker.py 
+start "" "C:\Program Files (x86)\NCSOFT\NC Launcher 2\Matryoshka.exe" /GameID:BnS
